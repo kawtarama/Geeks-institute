820b5bd0b1638ca07386a3d61f48bbe77db6cf30
kawtarama <140628656+kawtarama@users.noreply.github.com>
2025-05-05 18:15:41 +0100

    exercise

--- week1/day1/Exercises/Exercise1.py
+++ week1/day1/Exercises/Exercise1.py
@@ -1,14 +1,29 @@
 # 1/
-first = "Hello World"
-print(first)
-# 2/ Write a comment that says "This is a comment."
-# 3/
-print("I AM A COMPUTER!")
-# 4/
-if 1 < 2 and 4 > 2:
-    print("Math is fun.")
+print("Hello world\nHello world\nHello world\nHello world")
+# 2
+resultat = (99 ** 3) * 8
+print(resultat)
+
+# 3
+# my_name = "AMARA"  
+# nom_utilisateur = input("What's your name ? ")
+
+# if nom_utilisateur.lower() == my_name .lower():
+#     print("Wow! We have the same first name, that's awesome! ")
+# else:
+#     print(f"Delighted {nom_utilisateur} ! My first name is {my_name }.")
+
+
+# 4
+height = int(input("What is your height in centimeters? "))
+
+if height > 145:
+    print("You are tall enough to ride the roller coaster! ")
+else:
+    print("Sorry, you need to grow a bit more to ride! ")
+
+
+
 # 5
-nope = None
+
 # 6
-result = True and False  
-print(result)
