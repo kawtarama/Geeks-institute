710156dbca98b4f470559569749b9026b2c8a8a1
kawtarama <140628656+kawtarama@users.noreply.github.com>
2025-05-05 16:10:17 +0100

    f

---
+++ week1/day1/Exercises/Exercise1.py
@@ -0,0 +1,14 @@
+# 1/
+first = "Hello World"
+print(first)
+# 2/ Write a comment that says "This is a comment."
+# 3/
+print("I AM A COMPUTER!")
+# 4/
+if 1 < 2 and 4 > 2:
+    print("Math is fun.")
+# 5
+nope = None
+# 6
+result = True and False  
+print(result)
