b994e67e07536de8cc211ec0da161d775125afe4
kawtarama <140628656+kawtarama@users.noreply.github.com>
2025-06-01 23:28:09 +0100

    challenges

---
+++ week4/day5/Challenges/chal.js
@@ -0,0 +1,18 @@
+function isAnagram(str1, str2) {
+
+  const cleanStr1 = str1.replace(/\s+/g, '').toLowerCase();
+  const cleanStr2 = str2.replace(/\s+/g, '').toLowerCase();
+
+
+  const sortedStr1 = cleanStr1.split('').sort().join('');
+  const sortedStr2 = cleanStr2.split('').sort().join('');
+
+
+  return sortedStr1 === sortedStr2;
+}
+
+
+console.log(isAnagram("Astronomer", "Moon starer"));      
+console.log(isAnagram("School master", "The classroom"));  
+console.log(isAnagram("The Morse Code", "Here come dots"));
+console.log(isAnagram("Hello", "World"));                
